05e3acc0132acf39c77ffc47d87ac091d1077aa2
Timofey <kozulichev@gmail.com>
2021-04-05 17:24:39 +0300

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,14 @@
+public class Cat {
+    private String name;
+    private int age;
+
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return "Имя:" + name+ " возраст: "+ age;
+    }
+}
